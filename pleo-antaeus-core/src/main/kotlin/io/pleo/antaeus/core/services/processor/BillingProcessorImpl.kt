5de41b84dc2b86fd43a798d0f2c5ce547665e6ee
Daniel <danieldada123@gmail.com>
2022-03-29 10:32:31 +0100

    defined Processor State interface, Completed BillingProcessorImpl

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/processor/BillingProcessorImpl.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/processor/BillingProcessorImpl.kt
@@ -1,9 +1,21 @@
 package io.pleo.antaeus.core.services.processor
 
 import io.pleo.antaeus.core.services.processor.requestadapter.RequestAdapter
+import io.pleo.antaeus.core.services.processor.statechange.BillingProcessRequest
+import io.pleo.antaeus.core.services.processor.statechange.ProcessorState
+import io.pleo.antaeus.models.BillProcessorFlowState
+import mu.KotlinLogging
 
-class BillingProcessorImpl : BillingProcessor {
+class BillingProcessorImpl(private val map: Map<BillProcessorFlowState,ProcessorState>) : BillingProcessor {
+    private val logger = KotlinLogging.logger { }
+
     override fun process(requestAdapter: RequestAdapter) {
-
+        val request = BillingProcessRequest(requestAdapter,BillProcessorFlowState.START_STATE)
+        while (request.state != BillProcessorFlowState.STOP_STATE){
+            logger.info { ">> CURRENT STATE ==>==> (${request.state}).<<" }
+            val state = map[request.state]
+            state?.handleRequest(request);
+        }
     }
+
 }
